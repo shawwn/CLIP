10006cf053fe9d8502c4f95828c27e7009bea3e1
Shawn Presser <shawnpresser@gmail.com>
2021-01-08 09:31:09 -0800

    Add test_fetch.py

---
+++ clip/utils.py
@@ -0,0 +1,233 @@
+import requests
+import tqdm
+import os
+from urllib.parse import urlparse
+from contextlib import ExitStack
+
+ITER_CHUNK_SIZE = 512
+
+def is_remote_url(url):
+  info = urlparse(url)
+  return bool(info.scheme and info.netloc)
+
+def fetch_url_content_length(url):
+  if is_remote_url(url):
+    head = requests.head(url, headers={'Accept-Encoding': None})
+    return int(head.headers['Content-Length'])
+  else:
+    return os.path.getsize(url)
+
+def fetch_url_streaming(url):
+  if is_remote_url(url):
+    return requests.get(url, stream=True)
+  else:
+    return open(url, 'rb')
+
+def iter_lines(
+    iterator_or_url,
+    total_bytes=None,
+    decode_unicode=True,
+    chunk_size=ITER_CHUNK_SIZE,
+    progress_bar=True,
+    dynamic_ncols=True,
+    **tqdm_options):
+    """Iterates over the response data, one line at a time.  When
+    stream=True is set on the request, this avoids reading the
+    content at once into memory for large responses.
+
+    .. note:: This method is not reentrant safe.
+    """
+
+    url = None
+
+    with ExitStack() as stack:
+
+        if isinstance(iterator_or_url, str):
+            url = iterator_or_url
+            if total_bytes is None:
+                total_bytes = fetch_url_content_length(url)
+            iterator = fetch_url_streaming(url)
+
+        stack.enter_context(iterator)
+
+        if hasattr(iterator, 'iter_content'):
+            iterator = iterator.iter_content(chunk_size=chunk_size, decode_unicode=False)
+            #stack.enter_context(iterator)
+
+        pending = None
+        disable_progress_bar = total_bytes is None or not progress_bar
+
+        with tqdm.trange(total_bytes, disable=disable_progress_bar, dynamic_ncols=dynamic_ncols, **tqdm_options) as pbar:
+
+          def update(line):
+              pbar.update(len(line))
+              line = line.splitlines()
+              assert len(line) == 1
+              line = line[0]
+              return line
+
+          for chunk in iterator:
+              pbar.update(len(chunk))
+
+              if isinstance(chunk, bytes):
+                  chunk = chunk.decode('utf-8')
+
+              if pending is not None:
+                  chunk = pending + chunk
+
+              lines = chunk.splitlines(keepends=True)
+
+              if lines and lines[-1] and chunk and lines[-1][-1] == chunk[-1]:
+                  pending = lines.pop()
+              else:
+                  pending = None
+
+              for line in lines:
+                  yield update(line)
+
+          if pending is not None:
+              yield update(pending)
+
+
+class LineStream(ExitStack):
+    def close(self):
+        super().close()
+        getattr(self, 'lines', []).clear()
+        self.closed = True
+
+    def __call__(
+        self,
+        iterator_or_url,
+        total_bytes=None,
+        decode_unicode=True,
+        chunk_size=ITER_CHUNK_SIZE,
+        progress_bar=True,
+        dynamic_ncols=False,
+        unit_scale=1,
+        **tqdm_options):
+        """Iterates over the response data, one line at a time.  When
+        stream=True is set on the request, this avoids reading the
+        content at once into memory for large responses.
+
+        .. note:: This method is not reentrant safe.
+        """
+
+        if getattr(self, 'started', False):
+            raise RuntimeError("LineStream can't be started more than once")
+        self.started = True
+        self.url = None
+        self.closed = False
+        self.lines = []
+
+        with self:
+
+            if isinstance(iterator_or_url, str):
+                self.url = iterator_or_url
+                if total_bytes is None:
+                    total_bytes = fetch_url_content_length(self.url)
+                self.iterator = fetch_url_streaming(self.url)
+
+            self.enter_context(self.iterator)
+
+            if hasattr(self.iterator, 'iter_content'):
+                self.iterator = self.iterator.iter_content(chunk_size=chunk_size, decode_unicode=False)
+
+            self.pending = None
+            disable_progress_bar = total_bytes is None or not progress_bar
+
+            self.pbar = tqdm.tqdm(total=total_bytes, disable=disable_progress_bar, dynamic_ncols=dynamic_ncols, unit_scale=unit_scale, **tqdm_options)
+            self.enter_context(self.pbar)
+
+            def update(line):
+                self.pbar.update(len(line))
+                line = line.splitlines()
+                assert len(line) == 1
+                line = line[0]
+                if isinstance(line, bytes) and decode_unicode:
+                    line = line.decode('utf-8')
+                return line
+
+            for chunk in self.iterator:
+
+                if self.pending is not None:
+                    chunk = self.pending + chunk
+
+                lines = chunk.splitlines(keepends=True)
+
+                if lines and lines[-1] and chunk and lines[-1][-1] == chunk[-1]:
+                    self.pending = lines.pop()
+                else:
+                    self.pending = None
+
+                self.lines = lines
+                while True:
+                    try:
+                        line = self.lines.pop(0)
+                    except IndexError:
+                        if self.closed:
+                            return
+                        else:
+                            break
+                    yield update(line)
+
+            if self.pending is not None:
+                yield update(self.pending)
+
+
+
+# Python 3.5 backport. Is there a more elegant way to get a nullcontext?
+import abc
+
+
+class AbstractContextManager(abc.ABC):
+  """An abstract base class for context managers."""
+
+  def __enter__(self):
+    """Return `self` upon entering the runtime context."""
+    return self
+
+  @abc.abstractmethod
+  def __exit__(self, exc_type, exc_value, traceback):
+    """Raise any exception triggered within the runtime context."""
+    return None
+
+
+class nullcontext(AbstractContextManager):
+    """Context manager that does no additional processing.
+    Used as a stand-in for a normal context manager, when a particular
+    block of code is only sometimes used with a normal context manager:
+    cm = optional_cm if condition else nullcontext()
+    with cm:
+        # Perform operation, using optional_cm if condition is True
+    """
+
+    def __init__(self, enter_result=None):
+        self.enter_result = enter_result
+
+    def __enter__(self):
+        return self.enter_result
+
+    def __exit__(self, *excinfo):
+        pass
+
+
+class ProgressBar(AbstractContextManager):
+  def __init__(self, total_size, disable=False, **kws):
+    if not disable:
+      if not isinstance(total_size, int):
+        raise ValueError("Expected total_size to be set for progress bar")
+    self.total_size = total_size
+    self.disable = disable
+    #self.pbar = tqdm.trange(total_size, **kws) if not disable else nullcontext()
+    self.pbar = tqdm.trange(total_size, disable=disable, **kws)
+
+  def __enter__(self):
+    self.pbar.__enter__()
+    return self
+
+  def __exit__(self, *excinfo):
+    return self.pbar.__exit__(*excinfo)
+
+  def update(self, n):
+    if not self.disable:
+      return self.pbar.update(n)
---
+++ test_fetch.py
@@ -0,0 +1,92 @@
+# https://stackoverflow.com/questions/48483348/how-to-limit-concurrency-with-python-asyncio
+from random import randint
+import asyncio
+import tqdm
+import contextlib
+from clip import utils
+
+import httpx
+import asyncio
+import traceback
+
+from io import BytesIO
+import PIL.Image
+
+sem = asyncio.BoundedSemaphore(64)
+
+async def data_to_url_async(path):
+  async with sem:
+    data = filebytes(path)
+    orig = data
+    data = data_to_upload_image_args(data)
+    async with httpx.AsyncClient() as client:
+      try:
+        response = await client.post('https://staging.gather.town/api/uploadImage',
+            headers={'Content-Type': 'application/json'},
+            data=data,
+            timeout=httpx.Timeout(timeout=None))
+        if response.status_code == 200:
+          url = response.text
+          print(json_response(True, url, data=orig, path=path))
+          sys.stdout.flush()
+        else:
+          response.raise_for_status()
+      except Exception as caught:
+        report_error(caught, data=orig, path=path, code=response.status_code)
+
+
+async def process(callback, url, pbar, fake=False):
+  if fake:
+    wait_time = randint(1, 2)
+    pbar.write('downloading {} will take {} second(s)'.format(url, wait_time))
+    await asyncio.sleep(wait_time)  # I/O, context will switch to main function
+    pbar.write('downloaded {}'.format(url))
+  else:
+    async with httpx.AsyncClient() as client:
+      try:
+        response = await client.get(url, timeout=httpx.Timeout(timeout=None))
+        if response.status_code == 200:
+          await callback(None, response)
+        else:
+          await response.raise_for_status()
+      except Exception as caught:
+        #traceback.print_exc()
+        await callback(caught, response)
+        #report_error(caught, data=orig, path=path, code=response.status_code)
+        
+
+async def main(loop, url, concurrency=100):
+  with utils.LineStream() as stream:
+    received_bytes = 0
+    received_count = 0
+    failed_count = 0
+    dltasks = set()
+    async def callback(err, response):
+      nonlocal received_bytes, received_count, failed_count
+      if err is not None:
+        stream.pbar.write('{!r}: {!r}'.format(str(response.url), response))
+        failed_count += 1
+        return
+      received_count += 1
+      received_bytes += len(response.content)
+      image_bytes = response.content
+      image = PIL.Image.open(BytesIO(image_bytes))
+      url = str(response.url)
+      stream.pbar.write('Received {size} bytes: {url!r} {image!r}'.format(size=len(response.content), url=url, image=image))
+    for i, line in enumerate(stream(url)):
+        n = len(dltasks)
+        stream.pbar.set_description('%d in-flight / %d finished (%d failed) / %.2f MB [%s]' % (n, received_count, failed_count, received_bytes / (1024*1024), line.rsplit('/', 1)[-1]))
+        if len(dltasks) >= concurrency:
+            # Wait for some download to finish before adding a new one
+            _done, dltasks = await asyncio.wait(
+                dltasks, return_when=asyncio.FIRST_COMPLETED)
+        task = process(callback, line, pbar=stream.pbar)
+        dltasks.add(loop.create_task(task))
+    # Wait for the remaining downloads to finish
+    await asyncio.wait(dltasks)
+
+
+if __name__ == '__main__':
+  url = 'https://battle.shawwn.com/danbooru2019-s.txt'
+  loop = asyncio.get_event_loop()
+  loop.run_until_complete(main(loop, url))
