e5d362d7d2c988ac7af283daf491977ca93e2f60
Shawn Presser <shawnpresser@gmail.com>
2021-01-14 17:23:41 -0800

    Update test_fetch.py

--- test_fetch.py
+++ test_fetch.py
@@ -12,6 +12,11 @@ import traceback
 from io import BytesIO
 import PIL.Image
 
+from argparse import Namespace
+
+from urllib.parse import urlparse
+import os
+
 sem = asyncio.BoundedSemaphore(64)
 
 async def data_to_url_async(path):
@@ -35,7 +40,7 @@ async def data_to_url_async(path):
         report_error(caught, data=orig, path=path, code=response.status_code)
 
 
-async def process(callback, url, pbar, fake=False):
+async def process(callback, url, pbar, fake=False, timeout=60.0):
   if fake:
     wait_time = randint(1, 2)
     pbar.write('downloading {} will take {} second(s)'.format(url, wait_time))
@@ -44,13 +49,19 @@ async def process(callback, url, pbar, fake=False):
   else:
     async with httpx.AsyncClient() as client:
       try:
-        response = await client.get(url, timeout=httpx.Timeout(timeout=None))
+        response = await client.get(url, timeout=httpx.Timeout(timeout=timeout))
         if response.status_code == 200:
-          await callback(None, response)
+          try:
+            await callback(None, response)
+          except:
+            with tqdm.tqdm.external_write_mode(file=sys.stdout):
+              traceback.print_exc()
         else:
           await response.raise_for_status()
       except Exception as caught:
         #traceback.print_exc()
+        response = Namespace()
+        response.url = url
         await callback(caught, response)
         #report_error(caught, data=orig, path=path, code=response.status_code)
         
@@ -64,7 +75,7 @@ async def main(loop, url, concurrency=100):
     async def callback(err, response):
       nonlocal received_bytes, received_count, failed_count
       if err is not None:
-        stream.pbar.write('{!r}: {!r}'.format(str(response.url), response))
+        #stream.pbar.write('Failed: {!r}: {!r}'.format(str(response.url), response))
         failed_count += 1
         return
       received_count += 1
@@ -72,7 +83,11 @@ async def main(loop, url, concurrency=100):
       image_bytes = response.content
       image = PIL.Image.open(BytesIO(image_bytes))
       url = str(response.url)
-      stream.pbar.write('Received {size} bytes: {url!r} {image!r}'.format(size=len(response.content), url=url, image=image))
+      #stream.pbar.write('Received {size} bytes: {url!r} {image!r}'.format(size=len(response.content), url=url, image=image))
+      u = urlparse(url)
+      path = u.netloc + u.path
+      #stream.pbar.write(os.path.join(u.netloc, u.path))
+      stream.pbar.write(path)
     for i, line in enumerate(stream(url)):
         n = len(dltasks)
         stream.pbar.set_description('%d in-flight / %d finished (%d failed) / %.2f MB [%s]' % (n, received_count, failed_count, received_bytes / (1024*1024), line.rsplit('/', 1)[-1]))
@@ -87,6 +102,9 @@ async def main(loop, url, concurrency=100):
 
 
 if __name__ == '__main__':
-  url = 'https://battle.shawwn.com/danbooru2019-s.txt'
+  import sys
+  args = sys.argv[1:]
+  url = args[0] if len(args) >= 1 else 'https://battle.shawwn.com/danbooru2019-s.txt'
+  concurrency = int(args[1]) if len(args) >= 2 else 100
   loop = asyncio.get_event_loop()
-  loop.run_until_complete(main(loop, url))
+  loop.run_until_complete(main(loop, url, concurrency=concurrency))
